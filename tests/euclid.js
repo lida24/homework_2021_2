dde53925a0ac197a9155480d7d200602ac4ccdb3
lida24 <lidalenkova204@gmail.com>
2021-09-01 18:32:27 +0300

    added task

--- tests/euclid.js
+++ tests/euclid.js
@@ -1,31 +1,36 @@
-'use strict';
 
 QUnit.module('Тестируем функцию euclid', function () {
-	QUnit.test('На входе всего одно число', function (assert) {
-		assert.strictEqual(euclid(1), 1, 'euclid(1) === 1');
-		assert.strictEqual(euclid(2), 2, 'euclid(2) === 2');
-		assert.strictEqual(euclid(7), 7, 'euclid(7) === 7');
-		assert.strictEqual(euclid(6006), 6006, 'euclid(6006) === 6006');
-	});
+  QUnit.test('Работает правильно с одним числом', function (assert) {
+    assert.strictEqual(euclid(34), 34, 'euclid(34) === 34');
+    assert.strictEqual(euclid(175), 175, 'euclid(175) === 175');
+    assert.strictEqual(euclid(1024), 1024, 'euclid(1024) === 1024');
+    assert.strictEqual(euclid(12580), 12580, 'euclid(6006) === 6006');
+  });
 
-	QUnit.test('Функция должна правильно находить НОД трёх натуральных чисел', function (assert) {
-		assert.strictEqual(euclid(1, 1, 1), 1, 'euclid(1, 1, 1) === 1');
-		assert.strictEqual(euclid(2, 2, 2), 2, 'euclid(2, 2, 2) === 2');
-		assert.strictEqual(euclid(13, 13, 26), 13, 'euclid(13, 13, 26) === 13');
-		assert.strictEqual(euclid(3, 7, 1), 1, 'euclid(3, 7, 1) === 1');
-		assert.strictEqual(euclid(7, 7, 13), 1, 'euclid(7, 7, 13) === 1');
-		assert.strictEqual(euclid(2, 14, 16), 2, 'euclid(2, 14, 16) === 2');
-		assert.strictEqual(euclid(7, 14, 21), 7, 'euclid(7, 14, 21) === 7');
-		assert.strictEqual(euclid(6006, 3738735, 51051), 3003, 'euclid(6006, 3738735, 51051) === 3003');
-	});
+  QUnit.test('Работает правильно с тремя числами', function (assert) {
+    assert.strictEqual(euclid(17, 34, 1), 1, 'euclid(17, 34, 1) === 1');
+    assert.strictEqual(euclid(13, 13, 13), 13, 'euclid(13, 13, 13 === 13');
+    assert.strictEqual(euclid(54, 6, 9), 3, 'euclid(54, 6, 9) === 3');
+    assert.strictEqual(euclid(8, 10, 15), 1, 'euclid(8, 10, 15) === 1');
+    assert.strictEqual(euclid(128, 2, 64), 2, 'euclid(128, 2, 64) === 2');
+    assert.strictEqual(euclid(55, 11, 77), 11, 'euclid(55, 11, 77) === 11');
+    assert.strictEqual(euclid(21, 49, 56), 7, 'euclid(21, 49, 56) === 7');
+    assert.strictEqual(euclid(12580, 150960, 239020), 12580,
+    'euclid(12580, 150960, 239020) === 12580');
+  });
 
-	QUnit.test('Функция должна правильно работать с любым количеством аргументов', function (assert) {
-		assert.strictEqual(euclid(1, 1, 1, 1, 1, 1, 1), 1);
+  QUnit.test('Работает правильно с любым количеством чисел', function (assert) {
+    assert.strictEqual(euclid(1, 1, 1, 1, 1, 1, 1), 1);
+    const n = 23;
+    assert.strictEqual(euclid(3 * n, 2 * n, 4 * n, 7 * n, n, 21 * n), n);
+    const tmp = [559099, 4689920, 850927256464, 57433476879, 1234567, 234567890, 9856257836259];
+    assert.strictEqual(euclid(...[...tmp, ...tmp, ...tmp, ...tmp, ...tmp]), euclid(...tmp));
+  });
 
-		const n = 17;
-		assert.strictEqual(euclid(3 * n, 2 * n, 4 * n, 7 * n, n, 21 * n), n);
-
-		const temp = [ 80325, 55275, 8746650, 3000000, 45672375, 225, 54675 ];
-		assert.strictEqual(euclid(...[ ...temp, ...temp, ...temp, ...temp, ...temp ]), euclid(...temp));
-	});
+  QUnit.test('Работает правильно как с отрицательными, так и с положительными числами',
+   function (assert) {
+    assert.strictEqual(euclid(-1, 1), 1, 'euclid(-1, 1) === 1');
+    assert.strictEqual(euclid(-3, 9), 3, 'euclid(-1, 1) === 3');
+    assert.strictEqual(euclid(-7, 49), 7, 'euclid(-7, 49) === 7');
+  });
 });
