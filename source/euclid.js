5597a694bfdb34574401f2745da2f1ed906e8f56
lida24 <lidalenkova204@gmail.com>
2021-09-01 23:05:32 +0300

    Added jsdoc

--- source/euclid.js
+++ source/euclid.js
@@ -1,11 +1,25 @@
 'use strict';
 
+/**
+ *
+ * This function finds gcd of 2 numbers
+ *
+ * @param {number} x - first number
+ * @param {number} y - second number
+ * @returns {number}
+ */
 function gcd(x, y) {
   if (x !== 0) {
     return gcd(y % x, x);
   } else return y;
 }
 
+/**
+ * This function finds gcd of 2 numbers
+ *
+ * @param  {...number} numbers - n numbers
+ * @returns {number}
+ */
 const euclid = (...numbers) => numbers.reduce(function (max, current) {
   return gcd(max, Math.abs(current));
 }, 0);
